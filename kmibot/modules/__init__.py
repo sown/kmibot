37b079530f69a625f2e4dabab949f59bcdeaf9c1
Dan Trickey <git@trickey.io>
2024-08-23 20:50:09 +0100

    Remove sown_meeting command

--- kmibot/modules/__init__.py
+++ kmibot/modules/__init__.py
@@ -2,8 +2,7 @@ from kmibot.modules.module import Module
 
 from .ferry import FerryModule
 from .pub import PubModule
-from .sown_meeting import SOWNMeetingModule
 
-MODULES = [FerryModule, PubModule, SOWNMeetingModule]
+MODULES = [FerryModule, PubModule]
 
 __all__ = ["MODULES", "Module"]
